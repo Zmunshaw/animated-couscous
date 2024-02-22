4c21b189bde651ebcdb060bec281ec5e5843f943
TheAngryWookiee <thethompson@live.ca>
2024-02-22 16:37:36 -0500

    Pages finished, js needs some work

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,14 +1,12 @@
-class Contact
-{
-    constructor(contactName = "", emailAddress = "", contactNumber = "", contactMessage = "")
-    {
-        this.contactName = contactName;
-        this.emailAddress = emailAddress;
-        this.contactNumber = contactNumber;
-        this.contactMessage = contactMessage;
+class User {
+    constructor(firstName, lastName, username, email, password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
 }
-
 
 
 
@@ -20,8 +18,8 @@ let app;
 (function(app){
 
     // Declare Function Variables here...
-    console.log("%cDeclaring Variables", "color: red;")
-    let contactObject = new Contact();
+
+
 
     /**
      * Variable initialization in this function
@@ -29,10 +27,10 @@ let app;
      */
     function Start()
     {
-       PageSwitcher();
-
-        
        
+
+        $("#loginButton").click(validateLogin); 
+        $("#registerButton").click(validateForm);
        
 
 
@@ -42,126 +40,93 @@ let app;
         Main();
     }
 
-    function PageSwitcher()
-    {
-        let name = window.location.pathname;
 
-       let pageName = name.substring(1, name.length - 5);
-
-       switch(pageName)
-        {
-            case "index":
-               DisplayHomePageContent();
-                break;
-            case "products":
-                DisplayProductsContent();
-                break;
-            case "services":
-                DisplayServicesContent();
-                break;
-            case "about":
-                DisplayAboutContent();
-                break;
-            case "contact":
-                DisplayContactContent();
-                break;
-            case "projects":
-                DisplayProjectsContent();
-                break;
-            default:
-                console.error("Landed in a page that is not defined");
-                break;
-        }
-
-        // add a class of active to the active link
-        $("#"+pageName).addClass("active");
-    }
-
-    function DisplayHomePageContent()
-    {
-        document.getElementById("home").className = "nav-item active";
-        $("button").click(()=>{
-            location.href = "projects.html";
-        });
-    }
-
-    function DisplayProductsContent()
-    {
-
-    }
-
-    function DisplayServicesContent()
-    {
-
-    }
-
-    function DisplayAboutContent()
-    {
-
-    }
-
-    function DisplayContactContent()
-    {
         function clearForm()
         {
             //document.getElementById("contactForm").reset();
             $("#contactForm")[0].reset();
         }
 
-        $("#contactName").change((e)=>
-        {
-            if( $("#contactName").length < 8)
-            {
-                console.log("Contact Name Too Short");
-            }
-            console.log("changed");
-        });
 
-        $("#contactName").focus((e)=>
-        {
-            $("#contactName").select();
-            console.log("focus");
-        });
+    function validateLogin(event) {
+        event.preventDefault();
 
+        var username = $("input[name='username']").val();
+        var password = $("input[name='password']").val();
 
-        $("#submitButton").click((e)=>
-        {
-            e.preventDefault();
-            let contactName = $("#contactName").val();
-            let emailAddress = $("#emailAddress").val();
-            let contactNumber = $("#contactNumber").val();
-            let contactMessage = $("#contactMessage").val();
-
-            console.log(`Contact Name: ${contactName}`);
-            console.log(`Email Address: ${emailAddress}`);
-            console.log(`Contact Number: ${contactNumber}`);
-            console.log(`Contact Message: ${contactMessage}`);
-
-            contactObject.contactName = contactName;
-            contactObject.emailAddress = emailAddress;
-            contactObject.contactNumber = contactNumber;
-            contactObject.contactMessage = contactMessage;
-
-            console.log(contactObject);
-
-            clearForm();
-        });
-
-        $("#resetButton").click((e)=>
-        {
-            e.preventDefault();
-            if(confirm("Are You Sure?"))
-            {
-                clearForm();
-            }
-
+        if (isValidLogin(username, password)) {
             
-        });
+            insertUsername(username);
+            console.log("Login successful!");
+        } else {
+            console.log("Invalid username or password");
+        }
     }
 
-    function DisplayProjectsContent()
-    {
+    function isValidLogin(username, password) {
+ 
+        var validUser = new User("Lab2", "Lab2");
+        return username === validUser.username && password === validUser.password;
+    }
 
+    function insertUsername(username) {
+        
+        var usernameListItem = $('<li class="nav-item navbar-text">' + username + '</li>');
+        $("#loginLogoutLink").before(usernameListItem);
+    }
+
+    function validateForm(event) {
+
+        event.preventDefault();
+
+        var firstName = $("input[name='firstName']").val();
+        if (firstName.length < 2) {
+            displayErrorMessage("First Name must be at least 2 characters");
+            return;
+        }
+
+        var lastName = $("input[name='lastName']").val();
+        if (lastName.length < 2) {
+            displayErrorMessage("Last Name must be at least 2 characters");
+            return;
+        }
+        
+        var email = $("input[name='email']").val();
+        if (email.length < 8 || email.indexOf('@') === -1) {
+            displayErrorMessage("Email must be at least 8 characters long and contain '@'");
+            return;
+        }
+
+        var password = $("input[name='password']").val();
+        var confirmPassword = $("input[name='confirmPassword']").val();
+
+        if (password.length < 6 || password !== confirmPassword) {
+            displayErrorMessage("Passwords must match and be at least 6 characters long");
+            return;
+        }
+
+        var user = new User(
+            $("input[name='firstName']").val(),
+            $("input[name='lastName']").val(),
+            $("input[name='username']").val(),
+            $("input[name='email']").val(),
+            password
+        );
+
+        console.log("User Information:", user);
+  
+        hideErrorMessage();
+        clearForm();
+        console.log("Registration successful!");
+    }
+ 
+    function displayErrorMessage(message) {
+       
+        $("#ErrorMessage").text(message).show();
+    }
+    function hideErrorMessage() {
+        
+        $("#ErrorMessage").hide();
     }
 
     /**
